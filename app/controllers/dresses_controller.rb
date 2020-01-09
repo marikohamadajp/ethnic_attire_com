81c7ea27fd45b095ea5e383875a9013f02254c1d
Maik Froebisch <maik.froebisch@gmail.com>
2020-01-09 19:04:09 +0100

    added first crud to dress controller

--- app/controllers/dresses_controller.rb
+++ app/controllers/dresses_controller.rb
@@ -1,2 +1,24 @@
 class DressesController < ApplicationController
+  def index
+    @dresses = Dress.all
+  end
+
+  def new
+    @dress = Dress.new
+  end
+
+  def create
+    @dress = Dress.new(dress_params)
+    if @dress.save
+      # need to specify redirection after creation below
+      redirect_to root_path
+    else render :new
+    end
+  end
+
+  private
+
+  def dress_params
+    params.require(:cocktail).permit(:name, :photo)
+  end
 end
