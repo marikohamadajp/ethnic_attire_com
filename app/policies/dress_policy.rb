7d42d2db498ad75a4117a9797321d853f1cf6ed5
Veysi akin <weysi.akin1@gmail.com>
2020-01-10 18:00:59 +0100

    Authentication done

---
+++ app/policies/dress_policy.rb
@@ -0,0 +1,20 @@
+class DressPolicy < ApplicationPolicy
+
+  def index?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def update?
+    record.user == user
+    # - record: the restaurant passed to the `authorize` method in controller
+    # - user:   the `current_user` signed in with Devise.
+  end
+
+  def destroy?
+    record.user == user
+  end
+end
